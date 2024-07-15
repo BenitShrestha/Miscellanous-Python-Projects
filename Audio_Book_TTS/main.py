20b37db3ac340b7abc3c75e389a8a0af838dfcd8
BenitShrestha <quinjet01234@gmail.com>
2024-07-15 10:07:01 +0545

    Added user input to main.py and other changes
    Output audio files added

--- Audio_Book_TTS/main.py
+++ Audio_Book_TTS/main.py
@@ -2,7 +2,8 @@ import pyttsx3
 import pdfplumber
 
 # Open PDF in read binary mode
-with pdfplumber.open(r'Audio_Book_TTS\Text_Files\EnglishSpeaking.pdf') as book:  # Use raw string (r'') or escape backslashes (\\)
+bookname = input('Enter name of book: ')
+with pdfplumber.open(f'Audio_Book_TTS/Text_Files/{bookname}.pdf') as book:  # Use raw string (r'') or escape backslashes (\\)
     full_text = ""
     audio_reader = pyttsx3.init()
     audio_reader.setProperty('rate', 150)  # Adjust speech rate if needed
@@ -12,5 +13,6 @@ with pdfplumber.open(r'Audio_Book_TTS\Text_Files\EnglishSpeaking.pdf') as book: 
         full_text += content
         # audio_reader.say(content)
     
-    audio_reader.save_to_file(full_text, r'Audio_Book_TTS\Audio_Files\EnglishSpeaking.mp3')
+    audio_reader.save_to_file(full_text, f'Audio_Book_TTS/Audio_Files/{bookname}.mp3')
+    print(f'{bookname} PDF file convertd to audio file and saved')
     audio_reader.runAndWait()
