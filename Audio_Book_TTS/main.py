30814916d9df2df3b0cf77d03a64b36c62c94e81
BenitShrestha <quinjet01234@gmail.com>
2024-07-16 12:22:23 +0545

    Added Page to Page saving
    Audio files for each page of PDF is generated, further optimization need

--- Audio_Book_TTS/main.py
+++ Audio_Book_TTS/main.py
@@ -10,9 +10,13 @@ with pdfplumber.open(f'Audio_Book_TTS/Text_Files/{bookname}.pdf') as book:  # Us
 
     for page in book.pages:
         content = page.extract_text()
-        full_text += content
+
+        audio_reader.save_to_file(content, f'Audio_Book_TTS/Audio_Files/{bookname}_page{page.page_number}.mp3')
+        # full_text += content # Saves all contents to one variable, might cause issues
+
         # audio_reader.say(content)
-    
-    audio_reader.save_to_file(full_text, f'Audio_Book_TTS/Audio_Files/{bookname}.mp3')
-    print(f'{bookname} PDF file convertd to audio file and saved')
-    audio_reader.runAndWait()
+        audio_reader.runAndWait()
+    ''' To save whole book as one, for smaller PDFs '''
+    # audio_reader.save_to_file(full_text, f'Audio_Book_TTS/Audio_Files/{bookname}_Audio/{bookname}.mp3')
+    print(f'{bookname} PDF file converted to audio file and saved')
+    # audio_reader.runAndWait()
