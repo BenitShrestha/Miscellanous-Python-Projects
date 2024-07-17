c9a090e04a9e7f8d53144551a61eacdd4ed6c650
BenitShrestha <quinjet01234@gmail.com>
2024-07-17 09:35:41 +0545

    Added option to open files
    File options include *.txt, *.py

--- Simple-GUI-Text-Editor/main.py
+++ Simple-GUI-Text-Editor/main.py
@@ -15,9 +15,20 @@ class MyGUI(QMainWindow):
         self.action18_pt.triggered.connect(lambda: self.change_font_size(18))
         self.action24_pt.triggered.connect(lambda: self.change_font_size(24))
 
+        # Set action when user clicks on "Open"
+        self.actionOpen.triggered.connect(self.open_file)
+
     # Font-size change function, takes size as argument
     def change_font_size(self, size):
         self.plainTextEdit.setFont(QFont("Arial", size))
+    
+    # Open file function 
+    def open_file(self):
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt);;Python Files (*.py)", options = options)
+        if filename != "":
+            with open(filename, "r") as f:
+                self.plainTextEdit.setPlainText(f.read())
 
 def main():
     app = QApplication([])
