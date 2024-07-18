6f7adc96d364a2dd760814b34f7eaa6bf4faf6b0
BenitShrestha <quinjet01234@gmail.com>
2024-07-18 10:47:04 +0545

    Modularized audio merge script
    Imported in main script

---
+++ Audio_Book_TTS/audioMerge.py
@@ -0,0 +1,25 @@
+import os
+from pydub import AudioSegment
+from pydub.utils import which
+
+# AudioSegment.converter = which('ffmepg')
+# AudioSegment.ffprobe = which('ffprobe')
+
+def audio_merge(bookname):
+    audio_files_path = f'Audio_Book_TTS/Audio_Files/{bookname}/'
+
+    audio_files_list = [os.path.join(audio_files_path, name) for name in os.listdir(audio_files_path)]
+    # print(audio_files_list)    
+
+    first_file = AudioSegment.from_file(audio_files_list[0])
+    for audio_file in audio_files_list[1:]:
+        merged = AudioSegment.from_file(audio_file)
+        first_file += merged
+
+    first_file.export(f"Audio_Book_TTS/Merged_Files/{bookname}.mp3", format="mp3")
+    print(f"Audio files for {bookname} merged!")
+
+if __name__ == '__main__':
+    bookname = input("Insert bookname, so audio files can be merged: ")
+    audio_merge(bookname)
+
