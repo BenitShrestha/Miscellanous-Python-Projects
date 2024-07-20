d2b73fbe8497c145e6b7ccb70a6516d2ec08e50c
BenitShrestha <quinjet01234@gmail.com>
2024-07-20 20:53:52 +0545

    Modularized server.py, updated main.py
    Created a function called Bluetooth Chat, ran it from main.py

--- Bluetooth_Chat/main.py
+++ Bluetooth_Chat/main.py
@@ -1,1 +1,6 @@
 # Python Sockets
+
+from server import Bluetooth_Chat
+
+if __name__ == "__main__":
+    Bluetooth_Chat()
--- Bluetooth_Chat/server.py
+++ Bluetooth_Chat/server.py
@@ -5,27 +5,31 @@
 
 import socket 
 
-# Use socket.AI_INET for TCP
-server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) # RF COMM Protocol
+def Bluetooth_Chat():
+    # Use socket.AI_INET for TCP
+    server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) # RF COMM Protocol
 
-# Bind to MAC Address
-server.bind("e0:d4:e8:00:9d:cf", 4) # Bluetooth MAC Address, Specify a channel (20 Channels)
-server.listen(1) # Listen to 1 connection at a time
+    # Bind to MAC Address
+    server.bind(("e0:d4:e8:00:9d:cf", 4)) # Bluetooth MAC Address, Specify a channel (20 Channels)
+    server.listen(1) # Listen to 1 connection at a time
 
-# Client here is our client
-client, address = server.accept() # Accept incoming connection
+    # Client here is our client
+    client, address = server.accept() # Accept incoming connection
 
-try:
-    while True:
-        data = client.recv(1024) # Receive data from other client, 1024 bytes
-        if not data:
-            break
-        print(f"Message: {data.decode(data.decode('utf-8'))}") # Decode data using UTF-8
-        message = input("Enter Message: ")
-        client.send(message.encode('utf-8')) # Encode to bit stream
+    try:
+        while True:
+            data = client.recv(1024) # Receive data from other client, 1024 bytes
+            if not data:
+                break
+            print(f"Message: {data.decode(data.decode('utf-8'))}") # Decode data using UTF-8
+            message = input("Enter Message: ")
+            client.send(message.encode('utf-8')) # Encode to bit stream
 
-except OSError as e:
-    pass  
+    except OSError as e:
+        pass  
 
-client.close()
-server.close()
+    client.close()
+    server.close()
+
+if __name__ == "__main__":
+    Bluetooth_Chat()
