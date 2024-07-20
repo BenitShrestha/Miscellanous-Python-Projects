c173f39b9132e41f523a1e7aa517066cece55490
BenitShrestha <quinjet01234@gmail.com>
2024-07-20 20:52:51 +0545

    Added messaging functionality

--- Bluetooth_Chat/client.py
+++ Bluetooth_Chat/client.py
@@ -2,4 +2,18 @@
 import socket
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect("e0:d4:e8:00:9d:cf", 4) 
+client.connect(("e0:d4:e8:00:9d:cf", 4)) 
+
+try:
+    while True:
+        message = input("Enter message: ")
+        client.send(message.encode('utf-8'))
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message: {data.decode('utf-8')}")
+
+except OSError as e:
+    pass      
+
+client.close()
