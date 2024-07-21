59c6192233d8fb033a680221d45a02f67cb83c82
BenitShrestha <quinjet01234@gmail.com>
2024-07-21 20:56:41 +0545

    Added object drawing function
    Creates 2 paddles (rectangular) and a ball (circular)

--- Ping_Pong/main.py
+++ Ping_Pong/main.py
@@ -10,7 +10,7 @@ BLACK = (0, 0, 0)
 WIDTH =  600
 HEIGHT = 600
 
-pygame.innit()
+pygame.init()
 game_font = pygame.font.SysFont('monospace', 40)
 
 delay = 30 # Game pace, when increased game becomes slower
@@ -45,3 +45,20 @@ ball_y_pos = HEIGHT / 2
 ball_width =  8
 ball_x_vel = -10 # Goes left 
 ball_y_vel = 0 # Doesn't move vertically
+
+# Draw game components
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Create screen, pass tuple for size
+
+# Drawing objects 
+def draw_objects():
+    ''' Drawn on screen, color is white, int() used since division occurs, tuple passed with player position and bat dimensions'''
+    pygame.draw.rect(screen, WHITE, (int(p1_x_pos), int(p1_y_pos), paddle_width, paddle_height))
+    pygame.draw.rect(screen, WHITE, (int(p2_x_pos), int(p2_y_pos), paddle_width, paddle_height))
+    pygame.draw.circle(screen, WHITE, (int(ball_x_pos), int(ball_y_pos), ball_width)) 
+
+    # Scores 
+    score = game_font.render(f"{str(p1_score)} - {str(p2_score)}", False, WHITE)
+    screen.blit(score, (WIDTH / 2, 30)) # (X, Y)
+
+def apply_player_movement():
+    pass
