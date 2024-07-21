c8253fe64ebf3da37648bf245525c679478fbcf1
BenitShrestha <quinjet01234@gmail.com>
2024-07-21 21:28:03 +0545

    Added ball movement function
    Two scenarios, one where ball hits paddle either on left or right side and another where ball hits the wall - leading to a player gaining one score

--- Ping_Pong/main.py
+++ Ping_Pong/main.py
@@ -75,4 +75,33 @@ def apply_player_movement():
         p2_y_pos = min(p2_y_pos - paddle_speed, HEIGHT)
 
 def apply_ball_movement():
-    pass
+    global ball_x_pos, ball_y_pos, ball_x_vel, ball_y_vel, p1_score, p2_score
+
+    # First Scenario - Ball hits paddle on left, reverse X velocity so ball goes to right
+    if (ball_x_pos + ball_x_vel < p1_x_pos + paddle_width) and (p1_y_pos < ball_y_pos + ball_y_vel + ball_width < p1_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ''' If balls hits paddle in center (paddle_height / 2 == 0), no change in velocity, else goes up or down '''
+        ball_y_vel = (p1_y_pos + paddle_height / 2 - ball_y_pos) / 15 # Subject to change depending on physics
+        ball_y_vel = -ball_y_vel
+
+    # Second Scenario - Ball hits the wall 
+    elif ball_x_pos + ball_x_vel < 0: # Ball hits left wall
+        p2_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_y_pos = HEIGHT / 2
+        ball_x_vel = 10
+        ball_y_vel = 0 
+
+    # Identical code for another player
+    if (ball_x_pos + ball_x_vel > p2_x_pos - paddle_width) and (p2_y_pos < ball_y_pos + ball_y_vel < p2_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ball_y_vel = (p2_y_pos + paddle_height / 2 - ball_y_pos) / 15
+        ball_y_vel = -ball_y_vel
+    
+    elif ball_x_pos + ball_x_vel > HEIGHT:
+        p1_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_y_pos = HEIGHT / 2
+        ball_x_vel = -10
+        ball_y_vel = 0
+
